1869eec23a6a7900584bbf0f2b567988f2aac8d9
johan aji setiawan <johanajisetiawan1118@gmail.com>
2020-07-19 15:20:57 +0700

    sd

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // Set config defaults when creating the instance
 export const API = axios.create({
-  baseURL: "https://dumbsoundapp.herokuapp.com//api/v1",
+  baseURL: "https://dumbsoundapp.herokuapp.com/api/v1",
 });
 
 // Alter defaults after instance has been created
